fa9d776c2ba00650f9003b8877f993e70a230b6e
sultanmahmud07 <asadkhan01862@gmail.com>
2022-10-26 14:32:33 +0600

    data modifyed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/Category.json');
+const course =require('./data/Course.json');
 
 app.get('/', (req, res) => {
   res.send('BD API Runing!')
@@ -13,6 +14,29 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
   res.send(categories)
+});
+
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  if(id === '08'){
+    res.send(course)
+  }
+  else{
+    const category_course =course.filter( c => c.category_id === id);
+    res.send(category_course)
+
+  }
+})
+
+app.get('/course', (req, res) =>{
+  res.send(course);
+})
+
+app.get('/course/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = course.find(c => c._id === id);
+  res.send(selectedCourse)
+  // console.log(req.params.id);
 })
 
 app.listen(port, () => {
